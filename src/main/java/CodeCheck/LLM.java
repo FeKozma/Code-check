849aaf68a6d05dcea226ace0056d3a46097b4771
Felix Kozma <felix.kozma@gmail.com>
2024-02-15 20:02:16 +0100

    fix: configurational to run LLM

--- src/main/java/CodeCheck/LLM.java
+++ src/main/java/CodeCheck/LLM.java
@@ -2,8 +2,11 @@ package CodeCheck;
 
 import com.hexadevlabs.gpt4all.LLModel;
 
+import java.nio.file.Files;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.function.Supplier;
 
 
 public class LLM {
@@ -13,27 +16,51 @@ public class LLM {
     LLModel.GenerationConfig config;
 
     public void initModel() {
+        runIfConfig(() -> {
+            java.nio.file.Path modelPath = java.nio.file.Path.of(Util.LLM_FILE);
+            if (!Files.exists(modelPath)) {
+                Util.error("LLM_FILE expected to be configed");
+                throw new RuntimeException("LLM_FILE not found");
+            }
 
-        java.nio.file.Path modelPath = java.nio.file.Path.of("ggml-model-gpt4all-falcon-q4_0.bin");
+            model = new LLModel(modelPath);
 
-        model = new LLModel(modelPath);
+            config = LLModel.config()
+                    .withNPredict(4096).build();
+            return true;
+        });
 
-        config = LLModel.config()
-                .withNPredict(4096).build();
     }
 
-    public void cleanModel() throws Exception {
-        model.close();
+    public void cleanModel() {
+        runIfConfig(() -> {
+            try {
+                model.close();
+            } catch (Exception e) {
+                Util.error("LLM not closing");
+                throw new RuntimeException("LLM not closing", e);
+            }
+            return true;
+        });
     }
 
     public String getAnswer(String question) {
-        String answer = model.chatCompletion(createMessage(question), config).choices.toString();
-        System.out.println(answer);
-        return answer;
+        return runIfConfig(() -> {
+            String answer = model.chatCompletion(createMessage(question), config).choices.toString();
+            System.out.println(answer);
+            return answer;
+        }).orElse("LLM not configed");
     }
 
     private List<Map<String, String>> createMessage(String content) {
         return List.of(Map.of("role", "user", "content", content));
+    }
+
+    private <T> Optional<T> runIfConfig(Supplier<T> supplier) {
+        if (Util.RUN_WITH_LLM) {
+            return Optional.of(supplier.get());
+        }
+        return Optional.empty();
     }
 
 }
