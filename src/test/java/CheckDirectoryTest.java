06cfe0f6f0822080b7d748714ff8a7ce7b4bbf05
Jonathan Ström <jonathan.c.strom@gmail.com>
2024-02-20 16:39:02 +0100

    test: Added tests for using tilde at the start of a path to get the home path.

--- src/test/java/CheckDirectoryTest.java
+++ src/test/java/CheckDirectoryTest.java
@@ -1,11 +1,11 @@
 import CodeCheck.CheckDirectory;
 import CodeCheck.ManyFunctions;
+import CodeCheck.Util;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CheckDirectoryTest {
     @Test
@@ -14,5 +14,27 @@ public class CheckDirectoryTest {
         ManyFunctions functions = new CheckDirectory().checkDirectory(file);
 
         assertEquals(4, functions.oneFunctions.size());
+    }
+
+    @Test
+    public void checkIfCorrectPath() {
+        String file = new File(getClass().getResource("checkDirectoryTest1").getFile()).getPath();
+        assertEquals(file, Util.checkIfHomePath(file));
+    }
+
+    @Test
+    public void checkIfCorrectAbsolutePath() {
+        String file = new File(getClass().getResource("checkDirectoryTest1").getFile()).getAbsolutePath();
+        assertEquals(file, Util.checkIfHomePath(file));
+    }
+
+    @Test
+    public void checkIfHomePath() {
+        assertEquals(System.getProperty("user.home"), Util.checkIfHomePath("~"));
+    }
+
+    @Test
+    public void checkIfHomePathWithFolder() {
+        assertEquals(System.getProperty("user.home") + "/projects", Util.checkIfHomePath("~/projects"));
     }
 }
